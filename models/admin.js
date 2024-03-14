cdf5d65937d44ec16b7e9fa12f31465e68ddc0fb
SUJAL DINGANKAR <mailme.sujaldingankar@gmail.com>
2024-03-14 11:23:44 +0530

    new mr quiz backend

---
+++ models/admin.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    Name: String,
+    AdminId: {
+        type: String,
+        unique: true,
+    },
+    role: {
+        type: String,
+        enum: ['SUPER_ADMIN', 'CONTENT_ADMIN', 'REPORT_ADMIN', 1],
+        default: 1
+    }
+    ,
+    SUPER_ADMIN_COUNT: {
+        type: Number,
+        default: 0
+    }
+    ,
+    Password: String,
+    Gender: String,
+    MobileNumber: String,
+
+
+    Mrs: [
+        { type: mongoose.Schema.Types.ObjectId, ref: 'Mr' }
+    ]
+})
+
+module.exports = mongoose.model('admin', adminSchema)
